5178d61fffc94dbd29d56dda1592cd7975c44757
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-11-05 23:58:52 +0500

    Start

--- src/pages/Panler-bazasi/7-klass/Prezentaciyalar/videoo/index.tsx
+++ src/pages/Panler-bazasi/7-klass/Prezentaciyalar/videoo/index.tsx
@@ -10,21 +10,21 @@ export const PrezentatsiyaVideo7 = () => {
           className="mx-auto"
           src="https://www.youtube.com/embed/cwBpxwzNMd8"
           title="Elektr kernewi hám onı ólshew"
-          frameborder="0"
+          frameBorder={0}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
         ></iframe>
         <iframe
           width="789"
           height="444"
           className="mx-auto"
           src="https://www.youtube.com/embed/J5G1wN1oSyo"
-          title="Elektr kernewi hám onı ólshew"
-          frameborder="0"
+          title="Tok kúshi"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
         ></iframe>
       </div>
     </div>
