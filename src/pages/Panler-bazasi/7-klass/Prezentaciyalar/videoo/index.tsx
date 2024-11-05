c5a7fbbf3d9e3146f1353ca2a5407bf1a67f9701
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-11-05 23:50:08 +0500

    Start

--- src/pages/Panler-bazasi/7-klass/Prezentaciyalar/videoo/index.tsx
+++ src/pages/Panler-bazasi/7-klass/Prezentaciyalar/videoo/index.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export const PrezentatsiyaVideo7 = () => {
   return (
-    <div>PrezentatsiyaVideo7</div>
-  )
-}
+    <div>
+      <div className="my-3">
+        <iframe
+          width="789"
+          height="444"
+          className="mx-auto"
+          src="https://www.youtube.com/embed/cwBpxwzNMd8"
+          title="Elektr kernewi hám onı ólshew"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          width="789"
+          height="444"
+          className="mx-auto"
+          src="https://www.youtube.com/embed/J5G1wN1oSyo"
+          title="Elektr kernewi hám onı ólshew"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  );
+};
